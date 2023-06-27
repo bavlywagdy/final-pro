df29569347bd044519dc0d9a5a49d7494513ef09
Bavly Wagdy <bavlywagdy80@gmail.com>
2023-06-27 12:45:23 +0200

    i add some new js and try to edit styles in rented section

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const sr = ScrollReveal ({
-    distance: '60px',
-    duration:2500,
-    delay:400,
+    distance: '30px',
+    duration:1250,
+    delay:200,
     reset:true
 })
 
 
-sr.reveal ('.text', {delay:200, origin:'top'})
-sr.reveal ('.container form', {delay:800, origin:'left'})
-sr.reveal ('.heading', {delay:800, origin:'top'})
-sr.reveal ('.ride-container .box', {delay:600, origin:'top'})
-sr.reveal ('.rented', {delay:600, origin:'top'})
-sr.reveal ('.services-container .box', {delay:600, origin:'top'})
-sr.reveal ('.about-container .box', {delay:600, origin:'top'})
-sr.reveal ('.reviews-container', {delay:600, origin:'top'})
-
+sr.reveal ('.text', {delay:100, origin:'top'})
+sr.reveal ('.container form', {delay:400, origin:'left'})
+sr.reveal ('.heading', {delay:400, origin:'top'})
+sr.reveal ('.ride-container .box', {delay:300, origin:'top'})
+sr.reveal ('.rented', {delay:300, origin:'top'})
+sr.reveal ('.services-container .box', {delay:300, origin:'top'})
+sr.reveal ('.services-container-2 .box', {delay:300, origin:'top'})
+sr.reveal ('.services-container-3 .box', {delay:300, origin:'top'})
+sr.reveal ('.about-container .about-text', {delay:300, origin:'top'})
+sr.reveal ('.reviews-container', {delay:300, origin:'top'})
 
 
 
@@ -30,3 +31,29 @@ window.addEventListener('DOMContentLoaded', () => {
       });
   });
 });
+
+
+
+// getting all the btns for rent cars
+const rentNowButtons = document.querySelectorAll('.serv-btn');
+
+// click eventlistener 
+rentNowButtons.forEach(button => {
+  button.addEventListener('click', rentCar);
+});
+
+// Renting a car
+function rentCar(event) {
+  
+  const car = event.target.closest('.box');
+
+  // Clone the car
+  const clonedCar = car.cloneNode(true);
+
+  // Removing the car from the old section
+  car.remove();
+
+  // Append the cloned to the new section
+  const rentedSection = document.querySelector('#rented');
+  rentedSection.appendChild(clonedCar);
+}
